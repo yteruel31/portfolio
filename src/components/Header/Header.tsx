79609cbaebb967bbc9bd6cd271e62744aac00fc5
Yoann TERUEL <yoann.teruel@gmail.com>
2023-08-30 23:15:49 +0200

    feat(Header): add background on scrolling

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,20 @@
+'use client';
+
 import styles from './Header.module.css';
 import { GithubIcon, LinkedInIcon } from '@/components/Icons';
+import { useWindowScroll } from '@/hooks/useWindowScroll';
+import { clsx } from 'clsx';
 
 const Header = () => {
+  const [scroll] = useWindowScroll();
+
   return (
-    <header className={styles.header}>
+    <header
+      className={clsx(
+        styles.header,
+        scroll.y !== 0 ? styles.header__scrolling : undefined,
+      )}
+    >
       <div className={styles.navbar}>
         <div className={styles.container}>
           <div className={styles.title}>
---
+++ src/hooks/useWindowScroll.ts
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+
+interface ScrollPosition {
+  x: number;
+  y: number;
+}
+
+function getScrollPosition(): ScrollPosition {
+  return typeof window !== 'undefined'
+    ? { x: window.pageXOffset, y: window.pageYOffset }
+    : { x: 0, y: 0 };
+}
+
+function scrollTo({ x, y }: Partial<ScrollPosition>) {
+  if (typeof window !== 'undefined') {
+    const scrollOptions: ScrollToOptions = { behavior: 'smooth' };
+
+    if (typeof x === 'number') {
+      scrollOptions.left = x;
+    }
+
+    if (typeof y === 'number') {
+      scrollOptions.top = y;
+    }
+
+    window.scrollTo(scrollOptions);
+  }
+}
+
+export function useWindowScroll() {
+  const [position, setPosition] = useState<ScrollPosition>({ x: 0, y: 0 });
+  window.addEventListener('scroll', () => setPosition(getScrollPosition()));
+  window.addEventListener('resize', () => setPosition(getScrollPosition()));
+
+  useEffect(() => {
+    setPosition(getScrollPosition());
+  }, []);
+
+  return [position, scrollTo] as const;
+}
